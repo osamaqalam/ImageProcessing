925cfc467c6d60e707e45e98c498b934bf61e669
Osama <osama.qalam@hotmail.com>
2025-12-19 19:50:10 -0500

    draw a triangle in the middle of a connection, resolves #21

--- Utilities/ArrowPathConverter.cs
+++ Utilities/ArrowPathConverter.cs
@@ -16,11 +16,36 @@ namespace ImageProcessing.App.Utilities
                 !(values[1] is FlowchartNodeViewModel target))
                 return Geometry.Empty;
 
-            // Calculate connection points considering node dimensions
-            var start = new Point(source.X, source.Y + source.Height / 2);
-            var end = new Point(target.X, target.Y - target.Height / 2);
+            double sourceBottom = source.Y + source.Height / 2;
+            double targetTop = target.Y - target.Height / 2;
 
-            return new LineGeometry(start, end);
+            // Main line
+            // Calculate connection points considering node dimensions
+            if ((parameter as string) == "Line")
+            {
+                return new LineGeometry(
+                    new Point(source.X, sourceBottom),
+                    new Point(target.X, targetTop)
+                );
+            }
+
+            // Arrowhead (triangle)
+            else if ((parameter as string) == "Triangle")
+            {
+                double arrowSize = 10;
+                // Calculate exact midpoint between source bottom and target top
+                var end = new Point(target.X, (sourceBottom + targetTop + arrowSize) / 2);
+                var left = new Point(end.X - arrowSize / 2, end.Y - arrowSize);
+                var right = new Point(end.X + arrowSize / 2, end.Y - arrowSize);
+
+                var triangle = new PathGeometry();
+                var figure = new PathFigure { StartPoint = end, IsClosed = true };
+                figure.Segments.Add(new LineSegment(left, true));
+                figure.Segments.Add(new LineSegment(right, true));
+                triangle.Figures.Add(figure);
+                return triangle;
+            }
+            return Geometry.Empty;
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
